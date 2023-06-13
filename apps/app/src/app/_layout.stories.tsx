2acb422652cb0e72cf1f134e12fa5ef439f12f55
Silthus <michael@reichenbach.in>
2023-06-13 20:57:53 +0200

    generate @nx/react-native:storybook-config

---
+++ apps/app/src/app/_layout.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta } from '@storybook/react-native';
+import { Root } from './_layout';
+
+const Story: Meta<typeof Root> = {
+  component: Root,
+  title: 'Root',
+};
+export default Story;
+
+export const Primary = {
+  args: {},
+};
---
+++ apps/app/src/app/index.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta } from '@storybook/react-native';
+import { Home } from './index';
+
+const Story: Meta<typeof Home> = {
+  component: Home,
+  title: 'Home',
+};
+export default Story;
+
+export const Primary = {
+  args: {},
+};
